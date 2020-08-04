7cd94facdc8643b868e0617e81efba615bc1aa34
James Brown <jamesbrown5292@gmail.com>
2020-08-04 14:41:53 +0000

    after proving my design concept of drawing bars using html elements and css I have now succesfully created these inputs dynamically so that by using input to the console I can set the background nd draw the axes

---
+++ chart-generator.js
@@ -0,0 +1,18 @@
+//javascript file containing chart algorithm
+
+let drawChart = function(data, ){};
+
+let drawAxes = function(){
+  let body = document.querySelector("body")
+  let background  = document.createElement("div");
+  body.appendChild(background);
+  background.className = "background";
+  let yAxis = document.createElement("div");
+  yAxis.className = "line";
+  yAxis.id = "yAxis";
+  let xAxis = document.createElement("div");
+  xAxis.className = "line";
+  xAxis.id = "xAxis";
+  background.appendChild(yAxis);
+  background.appendChild(xAxis);
+}
